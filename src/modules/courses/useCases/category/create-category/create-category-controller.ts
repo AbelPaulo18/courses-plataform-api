97ce6864ccfff7d7edcfaf4b73eff581ffbd3289
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-06 21:38:12 +0100

    feat: :sparkles: setup and ensure create-category-controller executes and returns a response

---
+++ src/modules/courses/useCases/category/create-category/create-category-controller.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express'
+import { CreateCategoryUseCase } from './create-category-usecase'
+
+export class CreateCategoryController {
+	constructor(private createCategoryUseCase: CreateCategoryUseCase) {}
+
+	handle(request: Request, response: Response) {
+		const { name, description } = request.body
+
+		this.createCategoryUseCase.execute({ name, description })
+
+		return response.status(201).send()
+	}
+}
