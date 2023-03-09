570f325d9e79b3a19b050581dc8da4a214a25aa3
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-09 13:15:35 +0100

    refactor: :triangular_flag_on_post: update methods type

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -3,6 +3,6 @@ import { ICreateUserDTO } from '../dtos/ICreateUserDTO'
 
 export interface IUsersRepository {
 	create(data: ICreateUserDTO): Promise<void>
-	findByEmail(email: string): Promise<User>
-	findByPhoneNumber(phone: string): Promise<User>
+	findByEmail(email: string): Promise<User | null>
+	findByPhoneNumber(phone: string): Promise<User | null>
 }
