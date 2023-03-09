2e39d9a3d9b723fedf45e10d608e9e268ddab3f7
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-09 12:31:40 +0100

    feat: :sparkles: create usersRouter

---
+++ src/router/users.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express'
+import { createUserController } from '../modules/accounts/useCases/user/create-user'
+
+const usersRouter = Router()
+
+usersRouter.post('/', (request, response) => {
+	const createUser = createUserController
+
+	createUser.handle(request, response)
+})
+
+export { usersRouter }
