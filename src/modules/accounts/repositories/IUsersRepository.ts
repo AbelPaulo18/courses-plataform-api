03217c106c1ad602eb423d36c4e35a4645ec03a4
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-09 13:12:41 +0100

    feat: :sparkles: add findByEmail & findByPhoneNumber methods

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -3,4 +3,6 @@ import { ICreateUserDTO } from '../dtos/ICreateUserDTO'
 
 export interface IUsersRepository {
 	create(data: ICreateUserDTO): Promise<void>
+	findByEmail(email: string): Promise<User>
+	findByPhoneNumber(phone: string): Promise<User>
 }
