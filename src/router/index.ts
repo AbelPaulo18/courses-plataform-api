17fd3cd507d463230dec6a66d5600fbd90491d08
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-09 12:56:39 +0100

    feat: :sparkles: add userRouter

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import { categoriesRouter } from './categories.routes'
+import { usersRouter } from './users.routes'
 
 const router = Router()
 
 router.use('/categories', categoriesRouter)
+router.use('/users', usersRouter)
 
 export { router }
