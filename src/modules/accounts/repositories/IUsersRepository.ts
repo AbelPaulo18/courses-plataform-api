0b0d04f28a354696e00411fef5f9b8280208c791
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-10 19:15:30 +0100

    feat: :sparkles: add updateField method and Interface

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -1,8 +1,15 @@
 import { User } from '@prisma/client'
 import { ICreateUserDTO } from '../dtos/ICreateUserDTO'
 
+export interface IUpdateUserRequest {
+	user_id: string
+	data: any
+	column: 'name' | 'avatar' | 'email' | 'phone_number'
+}
+
 export interface IUsersRepository {
 	create(data: ICreateUserDTO): Promise<void>
+	updateField(data: IUpdateUserRequest): Promise<void>
 	findById(id: string): Promise<User | null>
 	findByEmail(email: string): Promise<User | null>
 	findByPhoneNumber(phone: string): Promise<User | null>
