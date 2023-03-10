00e6c317c158ee5ad10c42b39ab7b1c9e37bfc63
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-10 04:39:16 +0100

    feat: :sparkles: create finById method

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -3,6 +3,7 @@ import { ICreateUserDTO } from '../dtos/ICreateUserDTO'
 
 export interface IUsersRepository {
 	create(data: ICreateUserDTO): Promise<void>
+	findById(id: string): Promise<User | null>
 	findByEmail(email: string): Promise<User | null>
 	findByPhoneNumber(phone: string): Promise<User | null>
 }
