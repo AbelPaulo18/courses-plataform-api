99b26f657df904a777b9447eb5616e7fefe57689
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-10 08:10:35 +0100

    feat: :sparkles: add tryCatch block

--- src/modules/accounts/useCases/user/authenticate-user/authenticate-user-controller.ts
+++ src/modules/accounts/useCases/user/authenticate-user/authenticate-user-controller.ts
@@ -1,17 +1,25 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import { AuthenticateUserUseCase } from './authenticate-user-usecase'
 
 export class AuthenticateUserController {
 	constructor(private authenticateUserUseCase: AuthenticateUserUseCase) {}
 
-	async handle(request: Request, response: Response): Promise<Response> {
+	async handle(
+		request: Request,
+		response: Response,
+		next: NextFunction
+	): Promise<Response | void> {
 		const { password, email } = request.body
 
-		const token = await this.authenticateUserUseCase.execute({
-			password,
-			email,
-		})
+		try {
+			const token = await this.authenticateUserUseCase.execute({
+				password,
+				email,
+			})
 
-		return response.send(token)
+			return response.send(token)
+		} catch (error) {
+			next(error)
+		}
 	}
 }
