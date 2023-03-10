1a050188180f2f7e3cf091dd6c681975068282a3
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-10 19:17:52 +0100

    feat: :sparkles: create and set UpdateUserAvatarController

---
+++ src/modules/accounts/useCases/user/updateUserAvatar/update-user-avatar-controller.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from 'express'
+import { HttpCode } from '../../../../../errors/http-codes'
+import { UpdateUserAvatarUseCase } from './update-user-avatar-usecase'
+
+export class UpdateUserAvatarController {
+	constructor(private updateUserAvatarUseCase: UpdateUserAvatarUseCase) {}
+
+	async handle(
+		request: Request,
+		response: Response,
+		next: NextFunction
+	): Promise<Response | void> {
+		try {
+			const { id } = request.user
+			const avatar_file = request.file?.filename
+			//receber arquivo
+
+			await this.updateUserAvatarUseCase.execute({
+				user_id: id,
+				avatar_file,
+			})
+
+			return response.status(HttpCode.NO_CONTENT).send()
+		} catch (error) {
+			next(error)
+		}
+	}
+}
