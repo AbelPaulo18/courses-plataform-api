5238e20ec19c64b28fa589651486f800094245a2
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-10 05:03:50 +0100

    feat: :sparkles: implent error middleware

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 
 import { router } from './router'
 import swaggerUi from 'swagger-ui-express'
 
 import swaggerFile from '../swagger.json'
+import { AppError } from './errors/AppError'
 
 const server = express()
 
@@ -16,5 +17,19 @@ server.get('/', (request, response) => {
 		message: 'Hello World ❤️',
 	})
 })
+
+server.use(
+	(err: Error, request: Request, response: Response, next: NextFunction) => {
+		if (err instanceof AppError)
+			return response.status(err.statusCode).send({
+				message: err.message,
+			})
+
+		return response.status(500).send({
+			status: 'error',
+			message: `Internal Server Error - ${err.message}`,
+		})
+	}
+)
 
 server.listen(5000, () => console.log('Server Running...'))
