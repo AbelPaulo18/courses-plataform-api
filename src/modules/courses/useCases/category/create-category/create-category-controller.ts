7280b5eb1f1acc7b9db9b8725c7fc2bea5cd1938
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-10 08:14:49 +0100

    feat: :triangular_flag_on_post: add tryCatch block

--- src/modules/courses/useCases/category/create-category/create-category-controller.ts
+++ src/modules/courses/useCases/category/create-category/create-category-controller.ts
@@ -1,15 +1,23 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import { HttpCode } from '../../../../../errors/http-codes'
 import { CreateCategoryUseCase } from './create-category-usecase'
 
 export class CreateCategoryController {
 	constructor(private createCategoryUseCase: CreateCategoryUseCase) {}
 
-	async handle(request: Request, response: Response) {
-		const { name, description } = request.body
+	async handle(
+		request: Request,
+		response: Response,
+		next: NextFunction
+	): Promise<Response | void> {
+		try {
+			const { name, description } = request.body
 
-		await this.createCategoryUseCase.execute({ name, description })
+			await this.createCategoryUseCase.execute({ name, description })
 
-		return response.status(HttpCode.CREATED).send()
+			return response.status(HttpCode.CREATED).send()
+		} catch (error) {
+			next(error)
+		}
 	}
 }
