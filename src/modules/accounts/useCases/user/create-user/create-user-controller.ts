a4c7f84430cc26d037dfd2ab486ae29b457cf29b
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-10 07:56:04 +0100

    feat: :recycle: add tryCatch block

--- src/modules/accounts/useCases/user/create-user/create-user-controller.ts
+++ src/modules/accounts/useCases/user/create-user/create-user-controller.ts
@@ -1,20 +1,25 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
+import { AppError } from '../../../../../errors/AppError'
 import { HttpCode } from '../../../../../errors/http-codes'
 import { CreateUserUseCase } from './create-user-usecase'
 
 export class CreateUserController {
 	constructor(private createUserUseCase: CreateUserUseCase) {}
 
-	async handle(request: Request, response: Response) {
+	async handle(request: Request, response: Response, next: NextFunction) {
 		const { name, email, password, phone_number } = request.body
 
-		await this.createUserUseCase.execute({
-			email,
-			name,
-			password,
-			phone_number,
-		})
+		try {
+			await this.createUserUseCase.execute({
+				email,
+				name,
+				password,
+				phone_number,
+			})
 
-		response.status(HttpCode.CREATED).send()
+			response.status(HttpCode.CREATED).send()
+		} catch (error) {
+			next(error)
+		}
 	}
 }
