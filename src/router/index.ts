bc84ba6b0219407079db2a9632cf6b8847aee507
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-11 21:41:05 +0100

    feat: add coursesRouter to main Router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 import { authRoutes } from './authenticate.routes'
 import { categoriesRouter } from './categories.routes'
+import { coursesRouter } from './courses.routes'
 import { usersRouter } from './users.routes'
 
 const router = Router()
 
 router.use('/categories', categoriesRouter)
+router.use('/course', coursesRouter)
 router.use('/users', usersRouter)
 router.use('/auth', authRoutes)
 
