1b0aacc1721dac912207641aeb5c09e5987411fe
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-11 21:38:18 +0100

    feat: :sparkles: ensure courses DI

---
+++ src/modules/courses/useCases/courses/create-courses/index.ts
@@ -0,0 +1,11 @@
+import { CoursesRepository } from '../../../repositories/implementations/courses-repository'
+import { CreateCoursesController } from './create-courses-controller'
+import { CreateCoursesUseCase } from './create-courses-usecase'
+
+const coursesRepository = new CoursesRepository()
+const createCoursesUseCase = new CreateCoursesUseCase(coursesRepository)
+const createCoursesController = new CreateCoursesController(
+	createCoursesUseCase
+)
+
+export { createCoursesController }
