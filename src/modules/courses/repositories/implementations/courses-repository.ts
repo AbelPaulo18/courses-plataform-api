dc0fc9fdc0be534da3e8071a54c622ab7a8c754d
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-11 21:36:15 +0100

    feat: :sparkles: create and implement CoursesRepository

---
+++ src/modules/courses/repositories/implementations/courses-repository.ts
@@ -0,0 +1,55 @@
+import { Courses as courses } from '@prisma/client'
+
+import { prisma } from '../../../../prisma'
+import { ICoursesDTO } from '../../dtos/courses-dto'
+//import { Courses } from '../../entities/Courses'
+import { ICoursesRepository } from '../icourses-repository'
+
+export class CoursesRepository implements ICoursesRepository {
+	private repository
+
+	constructor() {
+		this.repository = prisma.courses
+	}
+
+	async create({
+		cover,
+		description,
+		duration,
+		name,
+		price,
+		category_id,
+	}: ICoursesDTO): Promise<void> {
+		await this.repository.create({
+			data: {
+				name,
+				cover,
+				description,
+				duration,
+				price,
+				category_id,
+			},
+		})
+	}
+
+	async findByName(name: string): Promise<courses | null> {
+		const course = await this.repository.findUnique({
+			where: { name },
+		})
+
+		return course
+	}
+
+	async list(): Promise<courses[]> {
+		const all_courses = await this.repository.findMany()
+
+		return all_courses
+	}
+
+	update(courses_id: string, data: any): Promise<void> {
+		throw new Error('Method not implemented.')
+	}
+	delete(courses_id: string): Promise<void> {
+		throw new Error('Method not implemented.')
+	}
+}
