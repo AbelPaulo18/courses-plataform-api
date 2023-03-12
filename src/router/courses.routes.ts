f3f105c50bfd81dc74d54360ee66a21faa3f62dd
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-12 03:47:28 +0100

    feat: :sparkles: add list courses route

--- src/router/courses.routes.ts
+++ src/router/courses.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express'
 import { createCoursesController } from '../modules/courses/useCases/courses/create-courses'
+import { listCoursesController } from '../modules/courses/useCases/courses/list-courses'
 
 const coursesRouter = Router()
 
 coursesRouter.post('/', (request, response, next) => {
 	createCoursesController.handle(request, response, next)
+})
+
+coursesRouter.get('/', (request, response, next) => {
+	listCoursesController.handle(request, response, next)
 })
 
 export { coursesRouter }
