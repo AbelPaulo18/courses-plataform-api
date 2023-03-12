d87d19a659ae0d2b20e54d198df2fe14ac163052
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-12 20:26:03 +0100

    feat: :sparkles: ensure create-chapter DI

---
+++ src/modules/courses/useCases/chapters/index.ts
@@ -0,0 +1,11 @@
+import { ChapterRepository } from '../../repositories/implementations/chapter-repository'
+import { CreateChapterController } from './create-chapter-controller'
+import { CreateChapterUseCase } from './create-chapter-usecase'
+
+const chapterRepository = new ChapterRepository()
+const createChapterUseCase = new CreateChapterUseCase(chapterRepository)
+const createChapterController = new CreateChapterController(
+	createChapterUseCase
+)
+
+export { createChapterController }
