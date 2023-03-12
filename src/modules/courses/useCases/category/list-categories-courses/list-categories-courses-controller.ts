ccae9d1921cd4305cd3bb2ee8510490d3c0b91f4
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-12 22:23:00 +0100

    feat: :sparkles: create and set ListCategoriesWithCoursesController

---
+++ src/modules/courses/useCases/category/list-categories-courses/list-categories-courses-controller.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from 'express'
+import { HttpCode } from '../../../../../errors/http-codes'
+import { ListCategoriesWithCoursesUseCase } from './list-categories-courses-usecase'
+
+export class ListCategoriesWithCoursesController {
+	constructor(
+		private listCategoryWithCoursesUseCase: ListCategoriesWithCoursesUseCase
+	) {}
+
+	async handle(request: Request, response: Response, next: NextFunction) {
+		try {
+			const categories = await this.listCategoryWithCoursesUseCase.execute()
+
+			return response.status(HttpCode.OK).send(categories)
+		} catch (error) {
+			next(error)
+		}
+	}
+}
