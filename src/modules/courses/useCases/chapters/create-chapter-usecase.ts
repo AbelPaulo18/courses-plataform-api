42663ae82ce534d255e334a8ce82efa219547adf
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-12 20:13:53 +0100

    feat: create and set CreateChapterUseCase

---
+++ src/modules/courses/useCases/chapters/create-chapter-usecase.ts
@@ -0,0 +1,21 @@
+import { type } from 'os'
+import { z } from 'zod'
+import { ChapterRepository } from '../../repositories/implementations/chapter-repository'
+
+const createChapterSchema = z.object({
+	name: z.string().min(3),
+	duration: z.number().min(0),
+	courses_id: z.string(),
+})
+
+type createChapterProps = z.infer<typeof createChapterSchema>
+
+export class CreateChapterUseCase {
+	constructor(private chapterRepository: ChapterRepository) {}
+
+	async execute(data: createChapterProps) {
+		const { name, duration, courses_id } = createChapterSchema.parse(data)
+
+		await this.chapterRepository.create({ name, duration, courses_id })
+	}
+}
