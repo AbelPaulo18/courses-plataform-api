be0d70e20aa031bf3d366b40e3716404139d2094
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-12 20:28:06 +0100

    feat: :sparkles: add chapterRouter to main router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { authRoutes } from './authenticate.routes'
 import { categoriesRouter } from './categories.routes'
+import { chapterRouter } from './chapter.routes'
 import { coursesRouter } from './courses.routes'
 import { usersRouter } from './users.routes'
 
@@ -8,6 +9,7 @@ const router = Router()
 
 router.use('/categories', categoriesRouter)
 router.use('/course', coursesRouter)
+router.use('/chapters', chapterRouter)
 router.use('/users', usersRouter)
 router.use('/auth', authRoutes)
 
