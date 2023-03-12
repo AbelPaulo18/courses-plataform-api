4ea0d6a5af434334abd3960752f2767714202fbd
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-12 03:42:27 +0100

    feat: :sparkles: create and set ListCoursesController

---
+++ src/modules/courses/useCases/courses/list-courses/list-courses-controller.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from 'express'
+import { HttpCode } from '../../../../../errors/http-codes'
+import { ListCoursesUseCase } from './list-courses-usecase'
+
+export class ListCoursesController {
+	constructor(private listCoursesUseCase: ListCoursesUseCase) {}
+
+	async handle(request: Request, response: Response, next: NextFunction) {
+		try {
+			const all_courses = await this.listCoursesUseCase.execute()
+
+			return response.status(HttpCode.OK).send(all_courses)
+		} catch (error) {
+			next(error)
+		}
+	}
+}
