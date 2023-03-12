b3588064d0f275263488be0c27b9625326589c82
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-12 05:26:27 +0100

    refactor: :triangular_flag_on_post: change duration validation type

--- src/modules/courses/useCases/courses/create-courses/create-courses-usecase.ts
+++ src/modules/courses/useCases/courses/create-courses/create-courses-usecase.ts
@@ -5,7 +5,7 @@ import { ICoursesRepository } from '../../../repositories/icourses-repository'
 const createCoursesSchema = z.object({
 	name: z.string().min(3),
 	description: z.string().min(3),
-	duration: z.string().min(3),
+	duration: z.number().min(3),
 	price: z.number().min(0),
 	cover: z.string(),
 	category_id: z.string(),
