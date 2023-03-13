5a948e4f682324d2fdd2476b8afe25f072aebffa
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-13 13:14:53 +0100

    feat: ensure ListUsers DI

---
+++ src/modules/accounts/useCases/user/list-users/index.ts
@@ -0,0 +1,9 @@
+import { UserRepository } from '../../../repositories/implementations/UsersRepositories'
+import { ListUsersController } from './list-users-controller'
+import { ListUsersUseCase } from './list-users-usecase'
+
+const userRepository = new UserRepository()
+const listUsersUseCase = new ListUsersUseCase(userRepository)
+const listUsersController = new ListUsersController(listUsersUseCase)
+
+export { listUsersController }
