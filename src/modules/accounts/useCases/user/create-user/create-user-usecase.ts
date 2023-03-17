c4c98ea0a80fe207804c21511506b6fdc866a3d0
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-17 11:57:00 +0100

    feat: :sparkles: implement CreateUserValidation

--- src/modules/accounts/useCases/user/create-user/create-user-usecase.ts
+++ src/modules/accounts/useCases/user/create-user/create-user-usecase.ts
@@ -3,17 +3,18 @@ import { AppError } from '../../../../../errors/AppError'
 import { ICreateUserDTO } from '../../../dtos/ICreateUserDTO'
 import { HashHelper } from '../../../helpers/hash/hash-helper'
 import { IUsersRepository } from '../../../repositories/IUsersRepository'
+import {
+	createUserProps,
+	createUserValidationSchema,
+} from '../../../validation/user/create-user-validation'
 
 export class CreateUserUseCase {
 	constructor(private userRepository: IUsersRepository) {}
 
-	async execute({
-		name,
-		email,
-		password,
-		phone_number,
-		role,
-	}: ICreateUserDTO): Promise<void> {
+	async execute(data: createUserProps): Promise<void> {
+		const { name, email, password, confirm_password, phone_number, role } =
+			createUserValidationSchema.parse(data)
+
 		const checkIfUserAlreadyExistsByEmail =
 			await this.userRepository.findByEmail(email)
 		const checkIfUserAlreadyExistsByPhoneNumber =
