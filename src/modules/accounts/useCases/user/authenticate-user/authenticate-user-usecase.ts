47bd5921e378fd6a0116712b8f2baaf94492e299
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-20 08:50:00 +0100

    feat: :sparkles: implement refresh_token logic

--- src/modules/accounts/useCases/user/authenticate-user/authenticate-user-usecase.ts
+++ src/modules/accounts/useCases/user/authenticate-user/authenticate-user-usecase.ts
@@ -1,6 +1,9 @@
+import auth from '@config/auth'
 import { AppError } from '@errors/AppError'
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository'
+import { IUserTokensRepository } from '@modules/accounts/repositories/IUserTokensRepository'
 import { compare } from 'bcrypt'
+import dayjs from 'dayjs'
 import { sign } from 'jsonwebtoken'
 
 interface IRequest {
@@ -15,30 +18,53 @@ interface IResponse {
 		phone_number: string
 	}
 	token: string
+	refresh_token: string
 }
 
 export class AuthenticateUserUseCase {
-	constructor(private userRepository: IUsersRepository) {}
+	constructor(
+		private userRepository: IUsersRepository,
+		private usersTokensRepository: IUserTokensRepository
+	) {}
 
 	async execute({ email, password }: IRequest): Promise<IResponse> {
-		//check if user exists...
 		const user = await this.userRepository.findByEmail(email)
+		const {
+			expires_in_token,
+			secret_refresh_token,
+			user_secret_token,
+			expires_in_refresh_token,
+			expires_in_refresh_token_days,
+		} = auth
+
+		const expires_date: Date = dayjs()
+			.add(expires_in_refresh_token_days, 'days')
+			.toDate()
 
 		if (!user) {
 			throw new AppError({ message: 'Email or password incorrect!' })
 		}
 
-		//verify if password its correct
 		const passwordMatch = await compare(password, user.password)
 
 		if (!passwordMatch) {
 			throw new AppError({ message: 'Email or password incorrect!' })
 		}
 
-		//generate jsonwebtoken
-		const token = sign({}, '664b47be371ce97aed289b894072bd892990d8bd', {
+		const token = sign({}, user_secret_token, {
 			subject: user.id,
-			expiresIn: '1d',
+			expiresIn: expires_in_token,
+		})
+
+		const refresh_token = sign({ email }, secret_refresh_token, {
+			subject: user.id,
+			expiresIn: expires_in_refresh_token,
+		})
+
+		await this.usersTokensRepository.create({
+			expires_date,
+			user_id: user.id,
+			refresh_token,
 		})
 
 		return {
@@ -48,6 +74,7 @@ export class AuthenticateUserUseCase {
 				name: user.name,
 			},
 			token,
+			refresh_token,
 		}
 	}
 }
