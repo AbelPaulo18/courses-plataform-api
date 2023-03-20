699f47b429c526d6b312cfaa5d3e32b8e967ea29
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-20 08:54:55 +0100

    feat: :sparkles: add /refresh-token route

--- src/shared/infra/http/router/authenticate.routes.ts
+++ src/shared/infra/http/router/authenticate.routes.ts
@@ -1,9 +1,14 @@
 import { authenticateUserController } from '@modules/accounts/useCases/user/authenticate-user'
+import { refreshTokenController } from '@modules/accounts/useCases/user/refresh-token'
 import { Router } from 'express'
 
 const authRoutes = Router()
 
 authRoutes.post('/sessions', (request, response, next) => {
 	authenticateUserController.handle(request, response, next)
+})
+
+authRoutes.post('/refresh-token', (request, response, next) => {
+	refreshTokenController.handle(request, response, next)
 })
 export { authRoutes }
