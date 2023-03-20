fb718425d3eb30c5f014278db6b1a5a2bd6e8a11
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-20 08:43:34 +0100

    feat: :closed_lock_with_key: create jwt-secrets

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+	user_secret_token: '664b47be371ce97aed289b894072bd892990d8bd',
+	secret_refresh_token: '07293fd2f793a4dae503479d4339ef0257bbb0b1',
+	expires_in_token: '15m',
+	expires_in_refresh_token: '30d',
+	expires_in_refresh_token_days: 30,
+}
