eb704e616ac85b59feff437456f43272260c2aa5
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-20 08:48:41 +0100

    feat: :sparkles: implement IUserTokenRepository contract

---
+++ src/modules/accounts/infra/prisma/repositories/UsersTokenRepositories.ts
@@ -0,0 +1,40 @@
+import { ICreateUserTokenDTO } from '@modules/accounts/dtos/ICreateUserTokenDTO'
+import { User } from '@modules/accounts/entities/User'
+import { UserTokens } from '@modules/accounts/entities/UserTokens'
+import { IUserTokensRepository } from '@modules/accounts/repositories/IUserTokensRepository'
+import { prisma } from '@shared/infra/prisma'
+
+export class UsersTokensRepositories implements IUserTokensRepository {
+	private repository = prisma.users_Tokens
+
+	async create({
+		user_id,
+		expires_date,
+		refresh_token,
+	}: ICreateUserTokenDTO): Promise<UserTokens> {
+		const userToken = await this.repository.create({
+			data: {
+				expires_date,
+				refresh_token,
+				user_id,
+			},
+		})
+
+		return userToken
+	}
+
+	async findByUserIdAndRefreshToken(
+		user_id: string,
+		refresh_token: string
+	): Promise<UserTokens> {
+		const usersTokens = await this.repository.findFirst({
+			where: { user_id, refresh_token },
+		})
+
+		return usersTokens
+	}
+
+	async deleteById(user_id: string): Promise<void> {
+		await this.repository.delete({ where: { id: user_id } })
+	}
+}
