778277df7ff318f5e30213fc17cb96f773eb5637
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-20 08:50:56 +0100

    refactor: :recycle: update authenticate DI

--- src/modules/accounts/useCases/user/authenticate-user/index.ts
+++ src/modules/accounts/useCases/user/authenticate-user/index.ts
@@ -1,10 +1,15 @@
 import { UserRepository } from '@modules/accounts/infra/prisma/repositories/UsersRepositories'
+import { UsersTokensRepositories } from '@modules/accounts/infra/prisma/repositories/UsersTokenRepositories'
 import { AuthenticateUserController } from './authenticate-user-controller'
 import { AuthenticateUserUseCase } from './authenticate-user-usecase'
 
 const userRepository = new UserRepository()
 
-const authenticateUserUseCase = new AuthenticateUserUseCase(userRepository)
+const usersTokensRepository = new UsersTokensRepositories()
+const authenticateUserUseCase = new AuthenticateUserUseCase(
+	userRepository,
+	usersTokensRepository
+)
 
 const authenticateUserController = new AuthenticateUserController(
 	authenticateUserUseCase
