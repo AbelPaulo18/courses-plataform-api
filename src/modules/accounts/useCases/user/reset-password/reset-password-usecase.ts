b20634c2b6247d81ba1b6f77c0c74b229bb95245
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-22 11:24:25 +0100

    feat: :sparkles: create and set ResetPasswordUseCase

---
+++ src/modules/accounts/useCases/user/reset-password/reset-password-usecase.ts
@@ -0,0 +1,40 @@
+import { hash } from 'bcrypt'
+
+import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository'
+import { IUserTokensRepository } from '@modules/accounts/repositories/IUserTokensRepository'
+import { AppError } from '@shared/errors/AppError'
+import { compareIfBefore, dateNow } from '@utils/dayjs-date-provider'
+
+interface IRequest {
+	token: string
+	password: string
+}
+
+export class ResetPasswordUseCase {
+	constructor(
+		private usersTokensRepository: IUserTokensRepository,
+		private userRespository: IUsersRepository
+	) {}
+
+	async execute({ password, token }: IRequest): Promise<void> {
+		const userToken = await this.usersTokensRepository.findByRefreshToken(token)
+
+		if (!userToken) throw new AppError({ message: 'Invalid Token 🤭' })
+
+		let now = new Date()
+		if (compareIfBefore(userToken.expires_date, now))
+			throw new AppError({ message: 'Token expired!🫤' })
+
+		const user = await this.userRespository.findById(userToken.user_id)
+
+		user.password = await hash(password, 12)
+
+		await this.userRespository.updateField({
+			column: 'password',
+			data: user.password,
+			user_id: user.id,
+		})
+
+		await this.usersTokensRepository.deleteById(userToken.id)
+	}
+}
