b2996e7c478c666d00d4288057a97c4476f56c20
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-22 11:33:03 +0100

    feat: :sparkles: add /reset route

--- src/shared/infra/http/router/password.routes.ts
+++ src/shared/infra/http/router/password.routes.ts
@@ -1,3 +1,4 @@
+import { resetPasswordController } from '@modules/accounts/useCases/user/reset-password'
 import { sendForgotPasswordMailController } from '@modules/accounts/useCases/user/send-forgot-password-mail'
 import { Router } from 'express'
 
@@ -5,6 +6,10 @@ const passwordRoutes = Router()
 
 passwordRoutes.post('/forgot', (request, response, next) => {
 	sendForgotPasswordMailController.handle(request, response, next)
+})
+
+passwordRoutes.post('/reset', (request, response, next) => {
+	resetPasswordController.handle(request, response, next)
 })
 
 export { passwordRoutes }
