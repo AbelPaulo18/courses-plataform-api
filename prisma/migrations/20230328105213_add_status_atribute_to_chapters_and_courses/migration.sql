21593cb5bb9042a7a46e1abaa92574292350a1b4
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-28 11:52:57 +0100

    refactor: :card_file_box: add status atribute to Courses and Chapters models

---
+++ prisma/migrations/20230328105213_add_status_atribute_to_chapters_and_courses/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "chapters" ADD COLUMN     "status" BOOLEAN NOT NULL DEFAULT false;
+
+-- AlterTable
+ALTER TABLE "courses" ADD COLUMN     "status" BOOLEAN NOT NULL DEFAULT false;
