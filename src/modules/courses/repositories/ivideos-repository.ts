6a74f4ac4b01fc1156406e5669daf6589f4a0102
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-28 15:41:36 +0100

    feat: :sparkles: create IVideosRepository

---
+++ src/modules/courses/repositories/ivideos-repository.ts
@@ -0,0 +1,10 @@
+import { IVideosDTO } from '../dtos/videos-dto'
+import { Videos } from '../entities/videos'
+
+export interface IVideosRepository {
+	create(data: IVideosDTO): Promise<void>
+	findByName(name: string): Promise<Videos>
+	listAll(): Promise<Videos[]>
+	updateById(id: string): Promise<void>
+	deleteById(id: string): Promise<void>
+}
