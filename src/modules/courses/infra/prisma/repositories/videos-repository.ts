fa4a715293ad4e29ea48513e71cae407d3ac6ca5
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-28 15:48:15 +0100

    feat: :sparkles: create and set VideoRepository methods

---
+++ src/modules/courses/infra/prisma/repositories/videos-repository.ts
@@ -0,0 +1,28 @@
+import { IVideosDTO } from '@modules/courses/dtos/videos-dto'
+import { Videos } from '@modules/courses/entities/videos'
+import { IVideosRepository } from '@modules/courses/repositories/ivideos-repository'
+import { prisma } from '@shared/infra/prisma'
+
+export class VideoRepository implements IVideosRepository {
+	repository = prisma.videos
+
+	async create(data: IVideosDTO): Promise<void> {
+		await this.repository.create({ data })
+	}
+	async findByName(name: string): Promise<Videos> {
+		const video = await this.repository.findFirst({ where: { name } })
+		return video
+	}
+
+	async listAll(): Promise<Videos[]> {
+		const allVideos = await this.repository.findMany()
+		return allVideos
+	}
+
+	updateById(id: string): Promise<void> {
+		throw new Error('Method not implemented.')
+	}
+	deleteById(id: string): Promise<void> {
+		throw new Error('Method not implemented.')
+	}
+}
