cc136a1ceb47b6c0331fee25fc233b43e2619ccf
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-31 14:03:16 +0100

    feat: :sparkles: create and add post videos Route

---
+++ src/shared/infra/http/router/video.routes.ts
@@ -0,0 +1,8 @@
+import { createVideoController } from '@modules/courses/useCases/videos'
+import { Router } from 'express'
+
+const videosRouter = Router()
+
+videosRouter.post('/', (request, response, next) => {
+	createVideoController.handle(request, response, next)
+})
