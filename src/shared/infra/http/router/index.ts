45291a9b494fb839998256a34189e627e9c11949
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-31 14:07:00 +0100

    feat: add videosRouter to main router

--- src/shared/infra/http/router/index.ts
+++ src/shared/infra/http/router/index.ts
@@ -5,12 +5,14 @@ import { chapterRouter } from './chapter.routes'
 import { coursesRouter } from './courses.routes'
 import { passwordRoutes } from './password.routes'
 import { usersRouter } from './users.routes'
+import { videosRouter } from './video.routes'
 
 const router = Router()
 
 router.use('/categories', categoriesRouter)
 router.use('/courses', coursesRouter)
 router.use('/chapters', chapterRouter)
+router.use('/videos', videosRouter)
 router.use('/users', usersRouter)
 router.use('/auth', authRoutes)
 router.use('/password', passwordRoutes)
