da04b9445c2e691dcf7c039d90a43259f6b9f116
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-31 13:31:27 +0100

    refactor: :recycle: rename method and its Props
    method to find a unique video

--- src/modules/courses/repositories/ivideos-repository.ts
+++ src/modules/courses/repositories/ivideos-repository.ts
@@ -3,7 +3,11 @@ import { Videos } from '../entities/videos'
 
 export interface IVideosRepository {
 	create(data: IVideosDTO): Promise<void>
-	findByName(name: string): Promise<Videos>
+	findByUnique(
+		name: string,
+		chapter_id: string,
+		number: number
+	): Promise<Videos>
 	findById(id: string): Promise<Videos>
 	listAll(): Promise<Videos[]>
 	updateById(id: string): Promise<void>
