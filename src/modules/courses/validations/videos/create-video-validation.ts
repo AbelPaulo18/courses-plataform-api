8b9ddb0cd01c6b0b41c3db4bed809178f4b08813
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-03-31 13:37:28 +0100

    refactor: add number field to Videos Schema

--- src/modules/courses/validations/videos/create-video-validation.ts
+++ src/modules/courses/validations/videos/create-video-validation.ts
@@ -4,6 +4,7 @@ const createVideoSchema = z.object({
 	name: z.string().min(3),
 	description: z.string().min(3).optional(),
 	chapter_id: z.string().min(3),
+	number: z.number().min(1),
 })
 
 type createVideoProps = z.infer<typeof createVideoSchema>
