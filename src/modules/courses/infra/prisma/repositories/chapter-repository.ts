0c2230d9e07693d712fd9997bdc5d483fad59c28
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-04-01 06:15:49 +0100

    feat: :sparkles: implement findByUnique method

--- src/modules/courses/infra/prisma/repositories/chapter-repository.ts
+++ src/modules/courses/infra/prisma/repositories/chapter-repository.ts
@@ -4,10 +4,17 @@ import { IChapterRepository } from '@modules/courses/repositories/ichapter-repos
 import { prisma } from '@shared/infra/prisma/index'
 
 export class ChapterRepository implements IChapterRepository {
-	private repository
+	private repository = prisma.chapters
 
-	constructor() {
-		this.repository = prisma.chapters
+	async findByUnique(number: number, courses_id: string): Promise<Chapter> {
+		return await this.repository.findUnique({
+			where: {
+				chapter_unique: {
+					courses_id,
+					number,
+				},
+			},
+		})
 	}
 
 	async create({ name, duration, courses_id }: IChapterDTO): Promise<void> {
