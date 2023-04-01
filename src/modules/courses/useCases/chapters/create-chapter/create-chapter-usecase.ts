afa9b1b933b518ef14cfbf33e65706d47fb40d2a
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-04-01 06:19:38 +0100

    refactor: change createChapterUseCase logic
    implement condition to check if the chapter already exists

--- src/modules/courses/useCases/chapters/create-chapter/create-chapter-usecase.ts
+++ src/modules/courses/useCases/chapters/create-chapter/create-chapter-usecase.ts
@@ -1,8 +1,9 @@
-import { ChapterRepository } from '@modules/courses/repositories/implementations/chapter-repository'
+import { ChapterRepository } from '@modules/courses/infra/prisma/repositories/chapter-repository'
 import {
 	createChapterProps,
 	createChapterSchema,
 } from '@modules/courses/validations/chapter/create-chapter-validation'
+import { AppError } from '@shared/errors/AppError'
 
 export class CreateChapterUseCase {
 	constructor(private chapterRepository: ChapterRepository) {}
@@ -10,6 +11,14 @@ export class CreateChapterUseCase {
 	async execute(data: createChapterProps) {
 		const { name, duration, courses_id, number } =
 			createChapterSchema.parse(data)
+
+		const chapterAlreadyExists = await this.chapterRepository.findByUnique(
+			number,
+			courses_id
+		)
+
+		if (chapterAlreadyExists)
+			throw new AppError({ message: 'Chapter already exists!' })
 
 		await this.chapterRepository.create({ name, duration, courses_id, number })
 	}
