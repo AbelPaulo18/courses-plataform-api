2231c79212d883bffe45af218f3ce9013a83662d
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-04-01 06:11:22 +0100

    feat: :sparkles: add new find method to IChaperRepository

--- src/modules/courses/repositories/ichapter-repository.ts
+++ src/modules/courses/repositories/ichapter-repository.ts
@@ -4,4 +4,5 @@ import { Chapter } from '../entities/chapter'
 export interface IChapterRepository {
 	create(data: IChapterDTO): Promise<void>
 	findByName(name: string): Promise<Chapter | null>
+	findByUnique(number: number, courses_id: string): Promise<Chapter | null>
 }
