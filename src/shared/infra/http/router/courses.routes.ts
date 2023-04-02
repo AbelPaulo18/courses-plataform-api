169d2afc5e1a202139b8de1d73de71d376b3f234
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-04-02 21:36:49 +0100

    feat: :sparkles: add new route
    /rel route returns courses count, chapters and reviews

--- src/shared/infra/http/router/courses.routes.ts
+++ src/shared/infra/http/router/courses.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { createCoursesController } from '@modules/courses/useCases/courses/create-courses'
 import { listCoursesController } from '@modules/courses/useCases/courses/list-courses'
+import { listCoursesChaptersController } from '@modules/courses/useCases/courses/list-courses-chapters'
 
 const coursesRouter = Router()
 
@@ -10,6 +11,10 @@ coursesRouter.post('/', (request, response, next) => {
 
 coursesRouter.get('/', (request, response, next) => {
 	listCoursesController.handle(request, response, next)
+})
+
+coursesRouter.get('/rel', (request, response, next) => {
+	listCoursesChaptersController.handle(request, response, next)
 })
 
 export { coursesRouter }
