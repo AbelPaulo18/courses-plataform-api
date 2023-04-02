e8fae39b82b39009848a2a3088c600a343bccf4d
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-04-02 21:18:14 +0100

    feat: :sparkles: add new find method
    this methods its responsible to get all related chapters and reviews

--- src/modules/courses/repositories/icourses-repository.ts
+++ src/modules/courses/repositories/icourses-repository.ts
@@ -4,6 +4,7 @@ import { Courses } from '../entities/courses'
 export interface ICoursesRepository {
 	create(data: ICoursesDTO): Promise<void>
 	findByName(name: string): Promise<Courses | null>
+	findCoursesChaptersRelation(): Promise<any>
 	list(): Promise<Courses[]>
 	update(courses_id: string, data: any): Promise<void>
 	delete(courses_id: string): Promise<void>
