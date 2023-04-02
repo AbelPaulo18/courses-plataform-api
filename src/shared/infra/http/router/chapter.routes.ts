a75e80c00036648edbd07d7318df86c485f18acb
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-04-03 00:13:54 +0100

    feat: :sparkles: add new route
    /rel its returns all chapters with related videos

--- src/shared/infra/http/router/chapter.routes.ts
+++ src/shared/infra/http/router/chapter.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { createChapterController } from '@modules/courses/useCases/chapters/create-chapter'
 import { listChaptersController } from '@modules/courses/useCases/chapters/list-chapters'
+import { listChaptersVideosController } from '@modules/courses/useCases/chapters/list-chapters-videos'
 
 const chapterRouter = Router()
 
@@ -10,6 +11,10 @@ chapterRouter.post('/', (request, response, next) => {
 
 chapterRouter.get('/', (request, response, next) => {
 	listChaptersController.handle(request, response, next)
+})
+
+chapterRouter.get('/rel', (request, response, next) => {
+	listChaptersVideosController.handle(request, response, next)
 })
 
 export { chapterRouter }
