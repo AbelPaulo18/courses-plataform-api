a520a068a44fec5c046b0066c7d7b61e85a951de
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-04-02 20:50:27 +0100

    feat: :sparkles: add new route to chapterRouter
    this route returns all available chapters

--- src/shared/infra/http/router/chapter.routes.ts
+++ src/shared/infra/http/router/chapter.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express'
 import { createChapterController } from '@modules/courses/useCases/chapters/create-chapter'
+import { listChaptersController } from '@modules/courses/useCases/chapters/list-chapters'
 
 const chapterRouter = Router()
 
 chapterRouter.post('/', (request, response, next) => {
 	createChapterController.handle(request, response, next)
+})
+
+chapterRouter.get('/all', (request, response, next) => {
+	listChaptersController.handle(request, response, next)
 })
 
 export { chapterRouter }
