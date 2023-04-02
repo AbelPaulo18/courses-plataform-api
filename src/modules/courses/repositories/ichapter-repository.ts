8665ddbe2167d24c43df3728cb2f806e4da63628
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-04-02 13:03:04 +0100

    feat: add new listAll method to list all chapters

--- src/modules/courses/repositories/ichapter-repository.ts
+++ src/modules/courses/repositories/ichapter-repository.ts
@@ -4,5 +4,6 @@ import { Chapter } from '../entities/chapter'
 export interface IChapterRepository {
 	create(data: IChapterDTO): Promise<void>
 	findByName(name: string): Promise<Chapter | null>
+	listAll(): Promise<Chapter[] | null>
 	findByUnique(number: number, courses_id: string): Promise<Chapter | null>
 }
